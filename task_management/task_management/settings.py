f14a74771ef276f1428ae444b445ed3ffe8259ab
tigistalexgech-git <tigistalexgech@gmail.com>
2026-03-02 09:49:18 +0300

    Task management deploymet

--- task_management/task_management/settings.py
+++ task_management/task_management/settings.py
@@ -105,8 +105,8 @@ DATABASES = {
     }
 }
 # If deploying to Heroku, replace the database with Heroku's PostgreSQL URL
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
